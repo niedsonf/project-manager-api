0d444893636e5c6e3384db07553b83d9f84efb7e
F7511299 Niedson Fernando Dantas Fernandes <niedson@bb.com.br>
2025-08-20 11:19:53 -0300

    :card_file_box: add repositories

--- src/infrastructure/database/repositories/tasks.repository.service.ts
+++ src/infrastructure/database/repositories/tasks.repository.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { DeepPartial, Repository } from 'typeorm';
+import { TaskEntity } from '../entities/task.entity';
+import { ITasksRepository } from 'src/domain/repositories/tasks-repository.interface';
+import { ITask } from 'src/domain/interfaces/task.interface';
+import { DataSource } from 'typeorm/browser';
 
 @Injectable()
-export class TasksRepositoryService {}
+export class TasksRepositoryService
+  extends Repository<TaskEntity>
+  implements ITasksRepository
+{
+  constructor(dataSource: DataSource) {
+    super(TaskEntity, dataSource.createEntityManager());
+  }
+
+  findAll(userId: number): Promise<ITask[]> {
+    return this.findBy({ user: { id: userId } });
+  }
+  findById(id: number): Promise<ITask | null> {
+    return this.findOneBy({ id });
+  }
+  add(payload: DeepPartial<ITask>): Promise<ITask> {
+    return this.save(payload);
+  }
+  updateById(id: number, payload: DeepPartial<ITask>) {
+    return this.update(id, payload);
+  }
+}
--- src/infrastructure/database/repositories/users.repository.service.ts
+++ src/infrastructure/database/repositories/users.repository.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { DeepPartial, Repository } from 'typeorm';
+import { UserEntity } from '../entities/user.entity';
+import { IUsersRepository } from 'src/domain/repositories/users-repository.interface';
+import { IUser } from 'src/domain/interfaces/user.interface';
+import { DataSource } from 'typeorm/browser';
 
 @Injectable()
-export class UsersRepositoryService {}
+export class UsersRepositoryService
+  extends Repository<UserEntity>
+  implements IUsersRepository
+{
+  constructor(dataSource: DataSource) {
+    super(UserEntity, dataSource.createEntityManager());
+  }
+
+  findById(id: number): Promise<IUser | null> {
+    return this.findById(id);
+  }
+  add(payload: DeepPartial<IUser>): Promise<IUser> {
+    return this.save(payload);
+  }
+}
